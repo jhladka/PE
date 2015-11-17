c620fee5c024248b5a58234bfdd7e04bbb9338c0
jhladka <jarka.repakova@centrum.cz>
2015-11-17 21:58:21 +0100

    initial commit : Euler project n.41

---
+++ 41-Pandigital-prime.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+
+from itertools import permutations
+from math import sqrt
+
+def is_prime(N):
+	sr = int(sqrt(N))
+	for i in range(2,sr+1):
+		if N%i == 0 : break
+	else: 	prime.append(N)
+
+prime = []
+for i in range(10,1,-1):
+	N = [str(j) for j in range(1,i)]
+	n = ''
+	for j in N : n += str(j) 
+	P = permutations(n)
+	for p in P : 
+		s=''
+		for j in p: s+=str(j)
+		is_prime(int(s))
+	if prime <> [] : break
+print max(prime)	
