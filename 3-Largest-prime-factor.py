8037f622ec9b8339c1aaf8298b5e34adbbf39151
jhladka <jarka.repakova@centrum.cz>
2015-11-17 21:56:13 +0100

    initial commit : Euler project n.3

---
+++ 3-Largest-prime-factor.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+    
+N = 600851475143
+
+def primes_less_than(m):
+    P = [2,3,5,7,11,13]
+    if m < 17 : return [x for x in P if x < m]
+    candidates = [x for x in xrange(17,m,2) if x%3 and x%5 and x%7 and x%11 and x%13]
+    candidates.reverse()
+    s = m**0.5
+    while candidates <> [] :
+        i = candidates.pop()
+        P.append(i)
+        if i <= s : candidates = filter(lambda x : x%i , candidates)
+        else : break
+    candidates.reverse()
+    P.extend(candidates)
+    return P
+
+P = primes_less_than(int(pow(N,0.5))) 
+
+n = -1
+while not N % P[n] == 0 : n -= 1
+print P[n]
