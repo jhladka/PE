039e3d9eb0f1aea9323bc4f3619b7ba88b578530
jhladka <jhladka@users.noreply.github.com>
2015-11-18 10:11:55 +0100

    Update 41-Pandigital-prime.py

--- 41-Pandigital-prime.py
+++ 41-Pandigital-prime.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python
 
+"""
+We shall say that an n-digit number is pandigital if it makes use of all the digits 1 to n exactly once. For example, 2143 is a 4-digit pandigital and is also prime.
+What is the largest n-digit pandigital prime that exists?
+"""
+
 from itertools import permutations
 from math import sqrt
 
