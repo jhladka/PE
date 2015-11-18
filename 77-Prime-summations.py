97a1931dc0c75b366a51a11e08ae8118b1bbb227
jhladka <jhladka@users.noreply.github.com>
2015-11-18 10:14:53 +0100

    Update 77-Prime-summations.py

--- 77-Prime-summations.py
+++ 77-Prime-summations.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python
+"""
+It is possible to write ten as the sum of primes in exactly five different ways:
+7 + 3
+5 + 5
+5 + 3 + 2
+3 + 3 + 2 + 2
+2 + 2 + 2 + 2 + 2
+What is the first value which can be written as the sum of primes in over five thousand different ways?
+"""
 
 def primes_less_than(m):
     P = [2,3,5,7,11,13]
